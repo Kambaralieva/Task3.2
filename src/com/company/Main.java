24306c17ecf48196f5e2ad69792655d232fa8601
Kambaralieva <Asemakambaralieva@gmail.com>
2020-08-12 22:03:21 +0600

    2nd option

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,33 @@
 package com.company;
 
-public class Main {
-    static double[] numbers = {0.0, 5.1, -3.1, 7.5, -11.3, -15.6,
-            20.8, 25.5, -30.4, 31.2, 96.7, -97.2, 98.2, -54.1, 55.1};
+import java.io.OutputStream;
 
+public class Main {
     public static void main(String[] args) {
-        double plusDigit = 0;
+        double[] numbers = {0.0, 5.1, -3.1, 7.5, -11.3, -15.6,
+                20.8, 25.5, -30.4, 31.2, 96.7, -97.2, 98.2, -54.1, 55.1};
+        double sum = 0.0;
+        boolean minusNumber = false;
+        double result;
+        int quantity = 0;
+
         for (double i : numbers) {
             if (i < 0) {
-                plusDigit++;
-                continue;
+                minusNumber = true;
             }
-            System.out.println(plusDigit += i);
+            if (minusNumber) {
+                if (i > 0) {
+                    sum += i;
+                    quantity++;
+                }
+            }
+
 
         }
-        System.out.println("Среднее ариф число");
-        System.out.println(plusDigit / 9);
+        result = sum / quantity;
+        System.out.println("Среднее значение" + " " + result);
 
     }
+
+
 }
